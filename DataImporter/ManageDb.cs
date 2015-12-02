e4de5c0e3273bb632b3b6ef1b8c76f826f6fbac1
wonpyo Hong <robinhwp@gmail.com>
2015-12-02 18:43:36 +0900

    초기작업

---
+++ DataImporter/ManageDb.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataImporter
+{
+    class ManageDb
+    {
+        SqlConnection m_conn = null;
+
+        bool Init( string info )
+        {
+            m_conn = new SqlConnection(info);
+            try
+            {
+                m_conn.Open();
+            }
+            catch( Exception ex )
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
