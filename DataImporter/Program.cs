18a9a1e8c475b128d939d1772f2b15f56a430e42
wonpyo Hong <robinhwp@gmail.com>
2015-12-04 11:03:08 +0900

    argument 정보 사용 추가

--- DataImporter/Program.cs
+++ DataImporter/Program.cs
@@ -11,12 +11,14 @@ namespace DataImporter
         // functions
         static void Main(string[] args)
         {
-            if(args.Length == 0)
+            if(args.Length < 6 )
             {
-                Console.WriteLine("Error : invalid file or directory.!");
+                Console.WriteLine("Error --  not enough parameters");
+                Console.WriteLine("Usage : DataImporter.exe file[path], ip, port, catalog, id, passwd");
                 Console.ReadKey();
                 return;
             }
+            DbManager.SetConnString(string.Format("Data Source={0},{1};Initial Catalog={2};USER ID={3};PASSWORD={4}", args[1], args[2], args[3], args[4], args[5]));
             // argument가 directory면 파일을 만들고
             string listFileName = "";
             System.IO.DirectoryInfo dirInfo = new System.IO.DirectoryInfo(args[0]);
